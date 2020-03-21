6c616514d03fef0757980530bdea00f4ced584e6
Manjinder Singh <manjinder.singh@schibsted.com>
2020-03-21 15:09:12 +0100

    remove test for main func

--- cmd/coverallssvc/main_test.go
+++ cmd/coverallssvc/main_test.go
@@ -31,18 +31,3 @@ func Test_sum(t *testing.T) {
 		})
 	}
 }
-
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "test",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}

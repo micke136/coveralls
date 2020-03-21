fe0620f41b337e61d86df0e4c86632f816644342
Manjinder Singh <manjinder.singh@schibsted.com>
2020-03-21 15:05:52 +0100

    add test for main func

--- cmd/coverallssvc/main_test.go
+++ cmd/coverallssvc/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_sum(t *testing.T) {
 	type args struct {
@@ -26,6 +28,21 @@ func Test_sum(t *testing.T) {
 			if got := sum(tt.args.x, tt.args.y); got != tt.want {
 				t.Errorf("sum() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
 		})
 	}
 }
